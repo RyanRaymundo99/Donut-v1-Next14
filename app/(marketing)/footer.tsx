5c32fa2d52f336bf94b74a304678178e9070e000
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2024-06-04 20:17:28 -0400

    footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
-    return (
-        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            footer
-        </footer>
-    )
-}
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/DE.svg"
+            alt="Germany"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          German
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/FR.svg"
+            alt="French"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+        French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/IT.svg"
+            alt="Italian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+        Italian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/ES.svg"
+            alt="Espanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Espanish
+        </Button>
+      </div>
+    </footer>
+  );
+};
