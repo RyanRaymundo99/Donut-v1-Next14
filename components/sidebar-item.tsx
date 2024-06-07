59df158f4ff06847688c518567911565774bb28a
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2024-06-06 20:51:17 -0400

    side bar and icons

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Props = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export const SidebarItem = ({ label, iconSrc, href }: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <Button
+      variant={active ? "sidebarOutlined" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+        <Image
+          src={iconSrc}
+          alt={label}
+          className="mr-5"
+          height={30}
+          width={30}
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,9 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { SidebarItem } from "./sidebar-item";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -8,11 +13,42 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 right-0 px-4 border-r-2 flex-col",
+        "flex bg-background h-full lg:w-[256px] lg:fixed left-0 right-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            alt="mascot / logo"
+            src="/mascot.png"
+            height={40}
+            width={40}
+            className="rounded-lg"
+          />
+          <h1 className="text-xl font-extrabold text-skye-500 tracking-wide">
+            Daqua
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem iconSrc="/house.png" label="Learn" href="/learn" />
+        <SidebarItem
+          iconSrc="/podium.png"
+          label="Leaderboard"
+          href="/leaderboard"
+        />
+        <SidebarItem iconSrc="/target.png" label="Quests" href="/quests" />
+        <SidebarItem iconSrc="/shop.png" label="Shop" href="/shop" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
