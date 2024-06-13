bdd263d90c0de6c0423b92338ef08ca3df382c30
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2024-06-13 01:27:28 -0400

    queries and drizzle config

---
+++ app/(main)/courses/card.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  title: string;
+  id: string;
+  imageSrc: string;
+  onClick: (id: number) => void;
+  disabled?: boolean;
+  active?: boolean;
+};
+
+export const Card = ({
+  title,
+  id,
+  imageSrc,
+  onClick,
+  disabled,
+  active,
+}: Props) => {
+    return (
+        <div onClick={}>
+            
+  </div>
+  
+    )
+};
+
+export default Card;
